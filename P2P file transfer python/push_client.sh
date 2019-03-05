8446e8119802124cc8ebdd0f05e2557c6c1cde2b
bhargavsundararajan <44955426+bhargavsundararajan@users.noreply.github.com>
2019-03-05 00:26:45 -0800

    delete wrong push_client

--- P2P file transfer python/push_client.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-gcloud compute firewall-rules create --network=NETWORK           \
-        default-allow-ssh --allow tcp:22
-gcloud compute ssh $1 --command "mkdir src; cd src/; exit"
-gcloud compute scp $2 $1:src/
